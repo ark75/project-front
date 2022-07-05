b81a2c232d15bc60df74f42dae3d32740af54919
ark75 <ark.pak75@gmail.com>
2022-07-05 18:01:30 +0300

    Initial step 2

--- src/main/webapp/css/Account.js
+++ src/main/webapp/css/Account.js
@@ -1,3 +1,23 @@
+import {
+    resetForm,
+    getAccounts,
+    currentPageIndex,
+    pagesNumber
+} from "/css/my.js";
+
+function deleteAcc(id) {
+
+    let url = "/rest/players/" + id;
+    $.ajax({
+        url: url,
+        type: "DELETE",
+        success: function () {
+            resetForm(currentPageIndex, pagesNumber);
+            getAccounts();
+        }
+    });
+}
+
 export default class Account {
     constructor(item) {
         this.id = item.id;
@@ -6,7 +26,7 @@ export default class Account {
         this.race = item.race;
         this.profession = item.profession;
         this.level = item.level;
-        this.birthday = item.birthday;
+        this.birthday = new Date(item.birthday);
         this.banned = item.banned;
     }
 
@@ -24,7 +44,23 @@ export default class Account {
         this.row.querySelector(".table__level").textContent = this.level;
         this.row.querySelector(".table__birthday").textContent = this.birthday;
         this.row.querySelector(".table__banned").textContent = this.banned;
+        let editImage = document.createElement("img");
+        editImage.src = "/img/edit.png";
+        editImage.id = this.id + "edit";
+        this.row.querySelector(".table__edit").append(editImage);
+        let deleteImage = document.createElement("img");
+        deleteImage.src = "/img/delete.png";
+        deleteImage.id = this.id + "delete";
+        $(deleteImage).click(function () {
+
+            deleteAcc(Number(deleteImage.id.charAt(0)));
+            resetForm();
+            currentPageIndex = document.querySelector(".button-active").textContent;
+            getAccounts(currentPageIndex, pagesNumber);
+        });
+        this.row.querySelector(".table__delete").append(deleteImage);
         return this.row;
     }
+
 
 }
